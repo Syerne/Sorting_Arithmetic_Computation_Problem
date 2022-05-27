4c49e200624b5157c09fc3b7648176a3eaafce21
Syerne <shubhamyerne1994@gmail.com>
2022-05-27 17:53:09 +0530

    Added: 2st calcultion part

--- sorting_Arithmetic_computation.sh
+++ sorting_Arithmetic_computation.sh
@@ -1,7 +1,9 @@
 echo "welcome to the sorting Arithmetic computation problems"
 
+part_time_hrs=8
 read -p "enter the value for a: " a
 read -p "enter the value for b: " b
 read -p "enter the value for c: " c
 
 A=$(( $a + $b * $c ))
+B=$(( $a * $b + $c ))
